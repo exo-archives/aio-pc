7237815038b131fe4f3265787572347d94d8acff
Alexey Zavizionov <alexey.zavizionov@exoplatform.com>
2008-07-22 16:27:03 +0000

    PC-283

--- deploy-with-del.sh
+++ deploy-with-del.sh
@@ -4,9 +4,9 @@
 
 cd applications/assembly/
 
-rm -r -f ../../../../../exo-working/exo-tomcat/
-mkdir ../../../../../exo-working/exo-tomcat/
-cp -r ../../../../../exo-dependencies/tomcat-6.0.13/* ../../../../../exo-working/exo-tomcat/
+rm -r -f ../../../../../../exo-working/exo-tomcat/
+mkdir ../../../../../../exo-working/exo-tomcat/
+cp -r ../../../../../../exo-dependencies/tomcat-6.0.16/* ../../../../../../exo-working/exo-tomcat/
 
 mvn clean exopc:deploy -Ddeploy=tomcat6
 
