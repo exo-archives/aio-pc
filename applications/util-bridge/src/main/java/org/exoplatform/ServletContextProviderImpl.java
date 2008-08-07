266811e0b7d49a0840b8199ed40105db3951b313
Alexey Zavizionov <alexey.zavizionov@exoplatform.com>
2008-08-07 15:14:23 +0000

    PC-257 	 Bad link URL for WSRP + struts portlet bridge

--- applications/util-bridge/src/main/java/org/exoplatform/ServletContextProviderImpl.java
+++ applications/util-bridge/src/main/java/org/exoplatform/ServletContextProviderImpl.java
@@ -9,25 +9,19 @@ import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 
+import org.apache.portals.bridges.common.ServletContextProvider;
 import org.exoplatform.services.portletcontainer.plugins.pc.portletAPIImp.PortletContextImpl;
 
-import org.apache.portals.bridges.common.ServletContextProvider;
+public class ServletContextProviderImpl implements ServletContextProvider {
+  public ServletContext getServletContext(GenericPortlet portlet) {
+    return ((PortletContextImpl) portlet.getPortletContext()).getWrappedServletContext();
+  }
 
+  public HttpServletRequest getHttpServletRequest(GenericPortlet portlet, PortletRequest request) {
+    return (HttpServletRequest) ((HttpServletRequestWrapper) request).getRequest();
+  }
 
-public class ServletContextProviderImpl implements ServletContextProvider
-{
-    public ServletContext getServletContext(GenericPortlet portlet)
-    {
-        return ((PortletContextImpl)portlet.getPortletContext()).getWrappedServletContext();
-    }
-
-    public HttpServletRequest getHttpServletRequest(GenericPortlet portlet, PortletRequest request)
-    {
-        return (HttpServletRequest) ((HttpServletRequestWrapper) request).getRequest();
-    }
-
-    public HttpServletResponse getHttpServletResponse(GenericPortlet portlet, PortletResponse response)
-    {
-        return (HttpServletResponse) ((HttpServletResponseWrapper) response).getResponse();
-    }
+  public HttpServletResponse getHttpServletResponse(GenericPortlet portlet, PortletResponse response) {
+    return (HttpServletResponse) ((HttpServletResponseWrapper) response).getResponse();
+  }
 }
