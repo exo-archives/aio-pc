4509c355e73a997c45628a266942b08a3608c342
Maxim Shaposhnik <max.shaposhnik@exoplatform.com>
2009-02-12 09:14:22 +0000

    PC-355;

--- component/plugins/wsrp2/src/main/java/org/exoplatform/services/wsrp2/bind/ext/WSRPV0ServiceAdministrationPortTypeImpl.java
+++ component/plugins/wsrp2/src/main/java/org/exoplatform/services/wsrp2/bind/ext/WSRPV0ServiceAdministrationPortTypeImpl.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2003-2009 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+
 package org.exoplatform.services.wsrp2.bind.ext;
 
 import java.util.HashMap;
@@ -29,7 +46,7 @@ public class WSRPV0ServiceAdministrationPortTypeImpl implements
   public void getServiceAdministration(javax.xml.ws.Holder<String> properties) {
 
     LOG.info("Executing operation getServiceAdministration");
-    System.out.println(properties);
+    LOG.info(properties);
 
     try {
 
@@ -44,10 +61,6 @@ public class WSRPV0ServiceAdministrationPortTypeImpl implements
       ex.printStackTrace();
       throw new RuntimeException(ex);
     }
-    //throw new ResourceSuspended("ResourceSuspended...");
-    //throw new InvalidRegistration("InvalidRegistration...");
-    //throw new ModifyRegistrationRequired("ModifyRegistrationRequired...");
-    //throw new OperationFailed("OperationFailed...");
   }
 
 }
