361b4924d3e466112ddfcb0bdedc348a3bbc4ff5
Maxim Shaposhnik <max.shaposhnik@exoplatform.com>
2009-02-24 14:09:45 +0000

    Path corr-s;

--- applications/assembly/deploy-tomcat5-with_console.bat
+++ applications/assembly/deploy-tomcat5-with_console.bat
@@ -1,6 +1,6 @@
-rmdir /s /q ..\..\..\..\..\..\..\exo-working\exo-tomcat\
-mkdir ..\..\..\..\..\..\..\exo-working\exo-tomcat\
-xcopy /e /h /r ..\..\..\..\..\..\..\exo-dependencies\tomcat-5.5.17\* ..\..\..\..\..\..\..\exo-working\exo-tomcat\*
+rmdir /s /q ..\..\..\..\..\..\exo-working\exo-tomcat\
+mkdir ..\..\..\..\..\..\exo-working\exo-tomcat\
+xcopy /e /h /r ..\..\..\..\..\..\exo-dependencies\tomcat-5.5.17\* ..\..\..\..\..\..\exo-working\exo-tomcat\*
 
 call mvn -f pom-jcr-console.xml clean exopc:deploy -Ddeploy=tomcat
 pause null
