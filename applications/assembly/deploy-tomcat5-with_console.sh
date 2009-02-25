40637b442b74d179504b5a4e0ceb83e73aa298bc
Alexey Zavizionov <alexey.zavizionov@exoplatform.com>
2009-02-25 10:50:22 +0000

    Several path modifications.

--- applications/assembly/deploy-tomcat5-with_console.sh
+++ applications/assembly/deploy-tomcat5-with_console.sh
@@ -1,6 +1,6 @@
-rm -r -f ../../../../../../exo-working/exo-tomcat/
-mkdir ../../../../../../exo-working/exo-tomcat/
-cp -r ../../../../../../exo-dependencies/tomcat-5.5.17/* ../../../../../../exo-working/exo-tomcat/
+rm -r -f ../../../../../exo-working/exo-tomcat/
+mkdir ../../../../../exo-working/exo-tomcat/
+cp -r ../../../../../exo-dependencies/tomcat-5.5.17/* ../../../../../exo-working/exo-tomcat/
 
 mvn -f pom-jcr-console.xml clean exopc:deploy -Ddeploy=tomcat
 
