3dd7b350b61694383d4e2c920b41975ff9794e4b
Alexey Zavizionov <alexey.zavizionov@exoplatform.com>
2009-03-23 09:01:41 +0000

    Modified depth of path for tomcat deploy.

--- build_and_deploy-with-del.sh
+++ build_and_deploy-with-del.sh
@@ -2,9 +2,9 @@ mvn clean install
 
 cd applications/assembly/
 
-rm -r -f ../../../../../exo-working/exo-tomcat/
-mkdir ../../../../../exo-working/exo-tomcat/
-cp -r ../../../../../exo-dependencies/tomcat-6.0.16/* ../../../../../exo-working/exo-tomcat/
+rm -r -f ../../../../../../exo-working/exo-tomcat/
+mkdir ../../../../../../exo-working/exo-tomcat/
+cp -r ../../../../../../exo-dependencies/tomcat-6.0.18/* ../../../../../../exo-working/exo-tomcat/
 
 mvn clean exopc:deploy -Ddeploy=tomcat6
 
