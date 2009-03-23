0d0c2871aae12a502c23c0c325dff6cc0e90b4e5
Alexey Zavizionov <alexey.zavizionov@exoplatform.com>
2009-03-23 14:22:45 +0000

    Renamed folder link within *.sh files: 'apache-tomcat-6.0.18' with 'tomcat-6.0.18'.

--- applications/assembly/deploy-test-wsrp2-tomcat6-withdelete.sh
+++ applications/assembly/deploy-test-wsrp2-tomcat6-withdelete.sh
@@ -4,7 +4,7 @@ rm -r -f ../../../../../exo-working/exo-tomcat/
 echo "Removed."
 mkdir ../../../../../exo-working/exo-tomcat/
 echo "Copying..."
-cp -r ../../../../../exo-dependencies/apache-tomcat-6.0.18/* ../../../../../exo-working/exo-tomcat/
+cp -r ../../../../../exo-dependencies/tomcat-6.0.18/* ../../../../../exo-working/exo-tomcat/
 echo "Copied."
 
 mvn -f pom-test-wsrp2.xml deploy -Ddeploy=tomcat6
